a94a64b628944c7599c62b4231ca994a05daf5fe
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-12-09 19:21:44 +0100

    simple backend server added

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
+import { Card } from 'react-bootstrap'
+
+const Product = ({ product }) => {
+	return (
+		<Card className="my-3 p-3 rounded">
+			<Link to={`/product/${product._id}`}>
+				<Card.Img src={product.image} variant="top"></Card.Img>
+			</Link>
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title as="div">
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
+				<Card.Text as="div">
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					></Rating>
+				</Card.Text>
+				<Card.Text as="h3" className="h3-product-price">
+					${product.price}
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	)
+}
+
+export default Product
