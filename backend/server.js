8cc3332719fad4bd687963d570185b2c35f35946
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-12-28 22:17:34 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,6 @@ if (process.env.NODE_ENV === 'development') {
 // Middleware for using json in a request body
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('API is running....')
-})
-
 //////////////////////////////////////////////////
 // Incorporate routes
 
@@ -52,9 +48,27 @@ app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+//////////////////////////////////////////////////
+// Set the uploads folder as static directory
+
 const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+//////////////////////////////////////////////////
+// Set the frontend build folder as static directory
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....')
+	})
+}
 
 //////////////////////////////////////////////////
 // Custom error handling via middleware
