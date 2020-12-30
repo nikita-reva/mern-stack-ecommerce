e0a2d890576fd08819e1f54cbe2a3a976b3c09d2
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-12-30 20:49:15 +0100

    Product countInStock is now decreased if an order is paid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -16,7 +16,6 @@ router.route('/').get(protect, admin, getOrders).post(protect, addOrderItems)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/:id/test').get(testStock)
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
